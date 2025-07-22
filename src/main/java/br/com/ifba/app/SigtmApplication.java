b63b5194e4b3b0d99ad9653e31314839e2570229
CaioPrimoDev <caioprimoedi@gmail.com>
2025-07-22 10:53:24 -0300

    git init

---
+++ src/main/java/br/com/ifba/app/SigtmApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifba.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigtmApplication.class, args);
+	}
+
+}
