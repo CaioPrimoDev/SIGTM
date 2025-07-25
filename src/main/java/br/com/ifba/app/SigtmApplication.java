9a674f26c9b796109364760863779c828ef66c70
Casa <Casa@DESKTOP-RKOHUUR>
2025-07-24 21:47:01 -0300

    teste commit

--- src/main/java/br/com/ifba/app/SigtmApplication.java
+++ src/main/java/br/com/ifba/app/SigtmApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // teste
+//TESTE PEDRO
 @SpringBootApplication
 public class SigtmApplication {
 
